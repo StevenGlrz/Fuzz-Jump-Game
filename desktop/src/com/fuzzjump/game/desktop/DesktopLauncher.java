5492b9af28a08f9bb23a51b057213624944c2b03
Stephen Moyer <steveadoo32@gmail.com>
2017-06-05 18:18:55 -0400

    Initial commit

---
+++ desktop/src/com/fuzzjump/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fuzzjump.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fuzzjump.game.FuzzJump;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FuzzJump(), config);
+	}
+}
